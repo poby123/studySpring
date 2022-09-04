4874132fbc8c9653b6ae08f6cbc57ded055d29c8
poby123 <i2pir@naver.com>
2022-09-04 20:22:20 +0900

    Feat: style

--- instagram/src/main/java/com/example/demo/api/controller/CommentApiController.java
+++ instagram/src/main/java/com/example/demo/api/controller/CommentApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +42,7 @@ public class CommentApiController {
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
     })
     @PostMapping
-    public CommentViewDto save(@Valid CommentSaveDto commentSaveDto) {
+    public CommentViewDto save(@RequestBody @Valid CommentSaveDto commentSaveDto) {
         Member member = userDetailsService.getMember("poby123")
                 .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
         Comment comment = commentService.save(member, commentSaveDto);
--- instagram/src/main/resources/static/js/index.js
+++ instagram/src/main/resources/static/js/index.js
@@ -1,6 +1,34 @@
 const cardContents = document.querySelectorAll('.text-truncate');
 cardContents.forEach((card) => {
     card.addEventListener('click', (e) => {
-        e.currentTarget.classList.remove('text-truncate');
+        e.currentTarget.classList.toggle('text-truncate');
     })
 })
+
+
+function commentSave(e) {
+    e.preventDefault();
+    const boardId = Number(document.querySelector('#boardId').value);
+    const content = document.querySelector('#commentContent').value;
+
+    const saveDto = {
+        'boardId': boardId,
+        'content': content
+    }
+
+    console.log(saveDto);
+
+    $.ajax({
+        type: "POST",
+        url: "http://wj-code-server.com:8080/api/comment",
+        data: JSON.stringify(saveDto),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: (data) => {
+            console.log(data);
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+}
